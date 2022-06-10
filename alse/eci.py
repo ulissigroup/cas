1ed57034da5b9c255396a619026c6755d58c98b9
LoryWang <xiaoxia3@andrew.cmu.edu>
2022-06-10 20:27:47 +0000

    update eci

--- alse/eci.py
+++ alse/eci.py
@@ -52,8 +52,8 @@ class ExpectedCoverageImprovement(MCAcquisitionFunction):
             num_samples: Number of samples for MC integration
         """
         super().__init__(model=model, objective=IdentityMCObjective(), **kwargs)
-#         assert len(constraints) == model.num_outputs
-#         assert all(direction in ("gt", "lt") for direction, _ in constraints)
+        assert len(constraints) == model.num_outputs
+        assert all(direction in ("gt", "lt") for direction, _ in constraints)
         assert punchout_radius > 0
         self.constraints = constraints
         self.punchout_radius = punchout_radius
@@ -82,54 +82,58 @@ class ExpectedCoverageImprovement(MCAcquisitionFunction):
 
     @property
     def train_inputs(self):
-        return self.model.train_inputs[0]
+        return self.model.models[0].train_inputs[0]
 
     def _generate_ball_of_points(
         self, num_samples, radius, device=None, dtype=torch.double
     ):
         """Creates a ball of points to be used for MC."""
         tkwargs = {"device": device, "dtype": dtype}
-        z = sample_hypersphere(d=self.dim, n=num_samples, qmc=True, **tkwargs)
+        z = sample_hypersphere(d=self.dim, n=num_samples, qmc=True, **tkwargs) # Not using self.dim
         r = torch.rand(num_samples, 1, **tkwargs) ** (1 / self.dim)
+        
         return radius * r * z
 
     def _get_base_point_mask(self, X):
-        print(X)
-        print(self.base_points)
-        distance_matrix = self.model.covar_module.base_kernel.covar_dist(
-            X, self.base_points
-        )
+        distance_matrix = self.model.models[0].covar_module.base_kernel.covar_dist(
+            X, self.base_points.double()
+        )   # Note to self: self.base_points is fp32
+            # Should standardize all to fp64?
         return smooth_mask(distance_matrix, self.punchout_radius)
 
     def _estimate_probabilities_of_satisfaction_at_points(self, points):
-        """Estimate the probability of satisfying the given constraints."""
-#         self.model.eval()
-#         posterior = self.model.models[0](points.double())
-        self.model.double()
-        print("points", points)
-        posterior = self.model.posterior(points)
-        mus, sigma2s = posterior.mean, posterior.variance
-        dist = torch.distributions.normal.Normal(mus, sigma2s.sqrt())
-        norm_cdf = dist.cdf(self._thresholds)
-        probs = torch.ones(points.shape[:-1]).to(points)
-        for i, (direction, _) in enumerate(self.constraints):
-            probs = probs * (
-                norm_cdf[..., i] if direction == "lt" else 1 - norm_cdf[..., i]
-            )
-        return probs
+        print("Entered estimate prob")
+        probabilities = torch.zeros((points.shape[0:2]))
+        for i in range(len(points)):
+            with gpytorch.settings.fast_pred_var(), torch.no_grad():
+                # print("Before test dist")
+                # print(f"self.model::::::: {self.model.posterior()[0]}")
+                # test_dist = self.model.posterior(points)
+                test_dist = model(points[i].float()) # Calculate posterior
+                # print("Before pred_means")
+                pred_means = test_dist.loc
+            pred_samples = test_dist.sample(torch.Size((50,))).exp()
+            prob_of_one_point = (pred_samples / pred_samples.sum(-2, keepdim=True))[:,1,:].mean(0)
+            probabilities[i] = prob_of_one_point
+        return probabilities
 
     @t_batch_mode_transform(expected_q=1)
     def forward(self, X):
         """Evaluate Expected Improvement on the candidate set X."""
-        print("candidateset", X)
-        print("self.ballofpoints", self.ball_of_points)
         ball_around_X = self.ball_of_points + X
         domain_mask = smooth_mask(
             ball_around_X, self.bounds[0, :], self.bounds[1, :]
         ).prod(dim=-1)
+        # print(f"domain mask: {domain_mask.shape}")
         num_points_in_integral = domain_mask.sum(dim=-1)
+        # print(f"num_points_in_integral: {num_points_in_integral.shape}")
+        # print("Right before base_point_mask")
+        # print(f"ball dtype: {ball_around_X.dtype}")
+        # print(f"ball shape: {ball_around_X.shape}")
         base_point_mask = self._get_base_point_mask(ball_around_X).prod(dim=-1)
+        # print(f"base_point_mask: {base_point_mask.shape}")
         prob = self._estimate_probabilities_of_satisfaction_at_points(ball_around_X)
+        # print(f"prob: {prob.shape}")
         masked_prob = prob * domain_mask * base_point_mask
         y = masked_prob.sum(dim=-1) / num_points_in_integral
         return y
