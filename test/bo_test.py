df1a022bf751e96c415937aa4fc97cab44f96417
LoryWang <xiaoxia3@andrew.cmu.edu>
2022-06-14 16:44:32 +0000

    add test function, modifu plot

--- test/bo_test.py
+++ test/bo_test.py
@@ -20,8 +20,6 @@ from alse.utils import smooth_mask, smooth_box_mask
 from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
 import random
 from alse.gp_model import DirichletGPModel
 from alse.eci import ExpectedCoverageImprovement
@@ -32,6 +30,9 @@ from gpytorch.kernels import ScaleKernel, RBFKernel
 from botorch.models.gpytorch import BatchedMultiOutputGPyTorchModel
 import random
 import torch
+from alse.test_function.fxn1 import yf
+from alse.plot import plot_acq_pos_gif
+import os
 
 SMOKE_TEST = os.environ.get("SMOKE_TEST")
 
@@ -88,36 +89,24 @@ def get_and_fit_gp(X, Y):
     return model
 
 
-lower_bound = -3
-upper_bound = 3
-bounds = torch.tensor(
-    [[lower_bound, lower_bound], [upper_bound, upper_bound]], **tkwargs
-)
-lb, ub = bounds
-dim = len(lb)
+lb = -3
+ub = 3
+bounds = torch.tensor([[lb, lb], [ub, ub]], **tkwargs)
 punchout_radius = 0.6
 threshold = 160
 num_init_points = 5
-num_total_points = 15
+num_total_points = 20
 
 # --------------------------------------- #
 # Setting up training points
-def yf(x):
-    v = (x[:, 0] ** 2 + x[:, 1] - 11) ** 2 + (x[:, 0] + x[:, 1] ** 2 - 7) ** 2
-    for i in range(len(v)):
-        if v[i] > threshold:
-            v[i] = 1
-        else:
-            v[i] = 0
-    return torch.stack((v, v), dim=-1)
 
 
-X = (lower_bound - upper_bound) * torch.rand(num_init_points, 2) + upper_bound
+X = (lb - ub) * torch.rand(num_init_points, 2) + ub
 X = X.float()
 Y = yf(X)
 # --------------------------------------- #
-
-
+if not os.path.exists("results"):
+    os.mkdir("results")
 while len(X) < num_total_points:
     # We don't have to normalize X since the domain is [0, 1]^2. Make sure to
     # appropriately adjust the punchout radius if the domain is normalized.
@@ -130,6 +119,17 @@ while len(X) < num_total_points:
         bounds=bounds,
         num_samples=50,
     )
+
+    plot_acq_pos_gif(
+        model=gp_models,
+        queried_pts_x=X,
+        queried_pts_y=Y,
+        num_init_points=num_init_points,
+        lb=lb,
+        ub=ub,
+        tkwargs=tkwargs,
+    )
+
     print("Checkpoint: x_next")
     # Switch to eval mode
     gp_models.eval()
@@ -146,78 +146,3 @@ while len(X) < num_total_points:
     X = torch.cat((X, x_next))
     Y = torch.cat((Y, y_next))
 
-N1, N2 = 50, 50
-Xplt, Yplt = torch.meshgrid(
-    torch.linspace(-3, 3, N1, **tkwargs), torch.linspace(-3, 3, N2, **tkwargs)
-)
-xplt = torch.stack(
-    (
-        torch.reshape(Xplt, (Xplt.shape[0] * Xplt.shape[1],)),
-        torch.reshape(Yplt, (Yplt.shape[0] * Yplt.shape[1],)),
-    ),
-    dim=1,
-)
-yplt = yf(xplt)
-Zplt = torch.reshape(yplt[:, 0], (N1, N2))
-
-
-def identify_samples_which_satisfy_constraints(X):
-    """
-    Takes in values (a1, ..., ak, o) and returns (a1, ..., ak, o)
-    True/False values, where o is the number of outputs.
-    """
-    successful = torch.ones(X.shape).to(X)
-    for model_index in range(X.shape[-1]):
-        these_X = X[..., model_index]
-        # direction, value = constraints[model_index]
-        # successful[..., model_index] = (
-        #     these_X < value if direction == "lt" else these_X > value
-        # )
-        successful[..., model_index] = these_X == 0
-    return successful
-
-
-fig, (ax, ax1) = plt.subplots(1, 2, figsize=(18, 6))
-h1 = ax.contourf(Xplt.cpu(), Yplt.cpu(), Zplt.cpu(), 20, cmap="Blues", alpha=0.6)
-fig.colorbar(h1, ax = ax)
-ax.contour(Xplt.cpu(), Yplt.cpu(), Zplt.cpu(), [-10, 0], colors="k")
-
-feasible_inds = (
-    identify_samples_which_satisfy_constraints(Y).prod(dim=-1).to(torch.bool)
-)
-ax.plot(X[feasible_inds, 0].cpu(), X[feasible_inds, 1].cpu(), "sg", label="Feasible")
-ax.plot(
-    X[~feasible_inds, 0].cpu(), X[~feasible_inds, 1].cpu(), "sr", label="Infeasible"
-)
-ax.scatter(
-    X.cpu()[:num_init_points, 0],
-    X.cpu()[:num_init_points, 1],
-    marker="o",
-    s=120,
-    color="k",
-    label="Training",
-)
-
-ind = 1
-for i in X[num_init_points:]:
-    ax.text(i[0], i[1], ind, size=15)
-    ind += 1
-ax.legend()
-ax.set_title("$f_1(x)$")  # Recall that f1(x) = f2(x)
-ax.set_xlabel("$x_1$")
-ax.set_ylabel("$x_2$")
-# ax.set_aspect("equal", "box")
-
-
-gp_models.eval()
-
-with gpytorch.settings.fast_pred_var(), torch.no_grad():
-    test_dist = gp_models(xplt.float())
-    pred_means = test_dist.loc
-pred_samples = test_dist.sample(torch.Size((256,))).exp()
-probabilities = (pred_samples / pred_samples.sum(-2, keepdim=True)).mean(0)
-ax1.contourf(Xplt.cpu(), Yplt.cpu(), pred_means.max(0)[1].reshape((N1,N2)))
-ax1.set_title("$posterior$") 
-ax1.set_xlabel("$x_1$")
-ax1.set_ylabel("$x_2$")
-plt.savefig("test.png")
