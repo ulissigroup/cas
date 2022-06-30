6f5d3e04c7ea3c2ab3a1dbc1ba0aaf7d1a80d85e
Leo-wz <leo1839@icloud.com>
2022-06-30 11:33:09 -0400

    compatibility fixes

--- alse/eci.py
+++ alse/eci.py
@@ -1,35 +1,17 @@
-import os
-import gpytorch
-from botorch.models.gpytorch import BatchedMultiOutputGPyTorchModel
-from botorch.models.utils import multioutput_to_batch_mode_transform
 import torch
+import gpytorch
 from botorch.acquisition.monte_carlo import MCAcquisitionFunction
 from botorch.acquisition.objective import IdentityMCObjective
-from botorch.fit import fit_gpytorch_model
-from botorch.models import ModelListGP, SingleTaskGP
-from botorch.models.transforms.outcome import Standardize
-from botorch.optim import optimize_acqf
 from botorch.utils.sampling import sample_hypersphere
 from botorch.utils.transforms import t_batch_mode_transform
-from gpytorch.constraints import Interval
-from gpytorch.likelihoods import GaussianLikelihood
-from gpytorch.mlls import ExactMarginalLogLikelihood
-from torch.quasirandom import SobolEngine
-from matplotlib import pyplot as plt
-from mpl_toolkits import mplot3d
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import random
-from gpytorch.models import ExactGP
-from gpytorch.likelihoods import DirichletClassificationLikelihood
-from gpytorch.means import ConstantMean
-from gpytorch.kernels import ScaleKernel, RBFKernel
 from alse.utils import smooth_mask, smooth_box_mask
-class ExpectedCoverageImprovement(MCAcquisitionFunction):
+from alse.gp_model import DirichletGPModel
+
+class ExpectedCoverageImprovement_regression(MCAcquisitionFunction):
     def __init__(
         self,
         model,
+        constraints,
         punchout_radius,
         bounds,
         num_samples=512,
@@ -51,7 +33,10 @@ class ExpectedCoverageImprovement(MCAcquisitionFunction):
             num_samples: Number of samples for MC integration
         """
         super().__init__(model=model, objective=IdentityMCObjective(), **kwargs)
+        assert len(constraints) == model.num_outputs
+        assert all(direction in ("gt", "lt") for direction, _ in constraints)
         assert punchout_radius > 0
+        self.constraints = constraints
         self.punchout_radius = punchout_radius
         self.bounds = bounds
         self.base_points = self.train_inputs
@@ -61,6 +46,9 @@ class ExpectedCoverageImprovement(MCAcquisitionFunction):
             device=bounds.device,
             dtype=bounds.dtype,
         )
+        self._thresholds = torch.tensor(
+            [threshold for _, threshold in self.constraints]
+        ).to(bounds)
         assert (
             all(ub > lb for lb, ub in self.bounds.T) and len(self.bounds.T) == self.dim
         )
@@ -75,35 +63,47 @@ class ExpectedCoverageImprovement(MCAcquisitionFunction):
 
     @property
     def train_inputs(self):
-        return self.model.train_inputs[0]
+        return self.model.models[0].train_inputs[0]
 
     def _generate_ball_of_points(
         self, num_samples, radius, device=None, dtype=torch.double
     ):
         """Creates a ball of points to be used for MC."""
         tkwargs = {"device": device, "dtype": dtype}
-        z = sample_hypersphere(d=self.dim, n=num_samples, qmc=True, **tkwargs) # Not using self.dim
+        z = sample_hypersphere(d=self.dim, n=num_samples, qmc=True, **tkwargs)
         r = torch.rand(num_samples, 1, **tkwargs) ** (1 / self.dim)
-        
         return radius * r * z
 
     def _get_base_point_mask(self, X):
-        distance_matrix = self.model.covar_module.base_kernel.covar_dist(
+        distance_matrix = self.model.models[0].covar_module.base_kernel.covar_dist(
             X, self.base_points.double()
-        )   # Note to self: self.base_points is fp32
-            # Should standardize all to fp64?
+        )
         return smooth_mask(distance_matrix, self.punchout_radius)
 
     def _estimate_probabilities_of_satisfaction_at_points(self, points):
-            print("Estimating probabilities")
-            probabilities = torch.zeros((points.shape[0:2]))
-            for i in range(len(points)):
-                with gpytorch.settings.fast_pred_var(), torch.no_grad():
-                    test_dist = self.model(points[i].float())
-                pred_samples = test_dist.sample(torch.Size((50,))).exp()
-                prob_of_one_point = (pred_samples / pred_samples.sum(-2, keepdim=True))[:,1,:].mean(0)
-                probabilities[i] = prob_of_one_point
-            return probabilities
+        """Estimate the probability of satisfying the given constraints."""
+        final_prob = torch.ones(points.shape[:-1])
+        for num in range(len(self.model.models)):
+            model = self.model.models[num]
+            if isinstance(model, DirichletGPModel):
+                probabilities = torch.ones(points.shape[:-1])
+                for i in range(len(points)):
+                    with gpytorch.settings.fast_pred_var(), torch.no_grad():
+                        test_dist = model(points[i].float())
+                    pred_samples = test_dist.sample(torch.Size((50,))).exp()
+                    prob_of_one_point = (pred_samples / pred_samples.sum(-2, keepdim=True))[:,1,:].mean(0)
+                    probabilities[i] = prob_of_one_point
+                final_prob =  final_prob*probabilities
+            else:
+                posterior = model.posterior(X=points)
+                mus, sigma2s = posterior.mean, posterior.variance
+                dist = torch.distributions.normal.Normal(mus, sigma2s.sqrt())
+                norm_cdf = dist.cdf(self._thresholds)
+                probs = torch.ones(points.shape[:-1]).to(points)
+                direction, _ = self.constraints[num]
+                probs = (norm_cdf[..., num] if direction == "lt" else 1 - norm_cdf[..., num])
+                final_prob = final_prob*probs
+        return final_prob
 
     @t_batch_mode_transform(expected_q=1)
     def forward(self, X):
--- alse/gp_model.py
+++ alse/gp_model.py
@@ -1,30 +1,9 @@
-import os
-import gpytorch
-from botorch.models.gpytorch import BatchedMultiOutputGPyTorchModel
-from botorch.models.utils import multioutput_to_batch_mode_transform
 import torch
-from botorch.acquisition.monte_carlo import MCAcquisitionFunction
-from botorch.acquisition.objective import IdentityMCObjective
-from botorch.fit import fit_gpytorch_model
-from botorch.models import ModelListGP, SingleTaskGP
-from botorch.models.transforms.outcome import Standardize
-from botorch.optim import optimize_acqf
-from botorch.utils.sampling import sample_hypersphere
-from botorch.utils.transforms import t_batch_mode_transform
-from gpytorch.constraints import Interval
-from gpytorch.likelihoods import GaussianLikelihood
-from gpytorch.mlls import ExactMarginalLogLikelihood
-from torch.quasirandom import SobolEngine
-from matplotlib import pyplot as plt
-from mpl_toolkits import mplot3d
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import random
+import gpytorch
 from gpytorch.models import ExactGP
-from gpytorch.likelihoods import DirichletClassificationLikelihood
 from gpytorch.means import ConstantMean
 from gpytorch.kernels import ScaleKernel, RBFKernel
+
 class DirichletGPModel(ExactGP):
     def __init__(self, train_x, train_y, likelihood, num_classes):
         super(DirichletGPModel, self).__init__(train_x, train_y, likelihood)
