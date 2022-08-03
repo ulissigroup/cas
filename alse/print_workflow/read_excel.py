d7955fa833266187c9cf9e507e32127e15c3ef58
Leo-wz <leo1839@icloud.com>
2022-08-03 15:31:38 -0400

    removed feed rate

--- alse/print_workflow/read_excel.py
+++ alse/print_workflow/read_excel.py
@@ -16,9 +16,9 @@ def read_excel():
 
     ############### Save this section for later ##############
     print("Name for the excel sheets are hardcoded in this version")
-    path1 = "..\\test_data\\4340_2.5mm_5.24gmin.xlsx"
-    path2 = "..\\test_data\\4340_3mm_5.24gmin.xlsx"
-    path3 = "..\\test_data\\4340_3mm_10.47gmin.xlsx"
+    path1 = "../test_data/4340_2.5mm_5.24gmin.xlsx"
+    path2 = "../test_data/4340_3mm_5.24gmin.xlsx"
+    path3 = "../test_data/4340_3mm_10.47gmin.xlsx"
     path = [path1, path2, path3]
     consolidated_data = []
     for i in range(3):
@@ -28,14 +28,14 @@ def read_excel():
     velocity = parse_data(consolidated_data, "V (mm/min)")
     # Hardcoded
     ones = torch.ones(40)
-    print("Spot size and feed rate are hardcoded in this version")
+    print("Spot size is hardcoded in this version")
     spot_size = torch.cat((ones*2.5, ones*3, ones*3))
-    feed_rate = torch.cat((ones*5.24, ones*5.24, ones*10.47))
+    # feed_rate = torch.cat((ones*5.24, ones*5.24, ones*10.47))
     #Output parameters
     width = parse_data(consolidated_data, "widths avg (mm)").unsqueeze(-1)
     pow_cap = (parse_data(consolidated_data, "powder capt %")/100).unsqueeze(-1)
     _height = parse_data(consolidated_data, "heights avg (mm)").bool().long().unsqueeze(-1)
     adhere = _height
 
-    X = torch.stack((power, velocity, spot_size, feed_rate), -1)
+    X = torch.stack((power, velocity, spot_size), -1)
     return X, width, pow_cap, adhere
