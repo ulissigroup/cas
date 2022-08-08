cfc83f567df60e8ab9713e758231028b38e30550
Leo-wz <leo1839@icloud.com>
2022-08-08 20:37:03 +0000

    test with old data

--- alse/print_workflow/read_excel_fixed_feed.py
+++ alse/print_workflow/read_excel_fixed_feed.py
@@ -16,25 +16,27 @@ def read_excel():
 
     ############### Save this section for later ##############
     print("Name for the excel sheets are hardcoded in this version")
-    path1 = "../test_data/8_4_data.xlsx"
-    # path2 = "../test_data/4340_3mm_5.24gmin.xlsx"
-    # path3 = "../test_data/4340_3mm_10.47gmin.xlsx"
-    path = [path1]
+    path1 = "../test_data/4340_2.5mm_5.24gmin.xlsx"
+    path2 = "../test_data/4340_3mm_5.24gmin.xlsx"
+    path3 = "../test_data/4340_3mm_10.47gmin.xlsx"
+
+    path = [path1, path2, path3]
     consolidated_data = []
-    consolidated_data.append(pandas.read_excel(f'{path[0]}').loc)    
+    for i in range(3):
+        consolidated_data.append(pandas.read_excel(f'{path[i]}').loc)   
     #Input parameters
     power = parse_data(consolidated_data, "P (W)")
     velocity = parse_data(consolidated_data, "V (mm/min)")
     # Hardcoded
-    # ones = torch.ones(40)
+    ones = torch.ones(40)
     # print("Spot size and feed rate are hardcoded in this version")
-    # spot_size = torch.cat((ones*2.5, ones*3, ones*3)) 
-    # feed_rate = torch.cat((ones*5.24, ones*5.24, ones*10.47))
+    spot_size = torch.cat((ones*2.5, ones*3, ones*3))
+    feed_rate = torch.cat((ones*5.24, ones*5.24, ones*10.47))
     #Output parameters
     # width = parse_data(consolidated_data, "widths avg (mm)").unsqueeze(-1)
-    pow_cap = (parse_data(consolidated_data, "powder_cap")/100).unsqueeze(-1)
-    width = (parse_data(consolidated_data, "width (mm)")).unsqueeze(-1)
-    height = (parse_data(consolidated_data, "height (mm)")).unsqueeze(-1)
+    pow_cap = (parse_data(consolidated_data, "powder capt %")/100).unsqueeze(-1)
+    width = parse_data(consolidated_data, "widths avg (mm)").unsqueeze(-1)
+    height = (parse_data(consolidated_data, "heights avg (mm)")).unsqueeze(-1)
     wth = width/height
     wth = torch.nan_to_num(wth, nan = torch.rand(1).item())
     # _height = parse_data(consolidated_data, "heights avg (mm)").bool().long().unsqueeze(-1)
@@ -42,5 +44,5 @@ def read_excel():
     # adhere = _height
     # width_to_height = width/height.unsqueeze(-1)
 
-    X = torch.stack((power, velocity), -1)
+    X = torch.stack((power, velocity, feed_rate, spot_size), -1)
     return X, width, pow_cap, wth
