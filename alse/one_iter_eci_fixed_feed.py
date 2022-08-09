aa5b17328593ae5e78a28d66e44beba853411c6f
Leo-wz <leo1839@icloud.com>
2022-08-09 15:57:43 +0000

    removed hard coded

--- alse/one_iter_eci_fixed_feed.py
+++ alse/one_iter_eci_fixed_feed.py
@@ -5,7 +5,7 @@ from alse.eci_fixed_feed import ExpectedCoverageImprovement
 from alse.print_workflow.init_model import initialize_models
 
 def one_iter_eci(X, constraints, punchout_radius, bounds, *Y):
-    list_of_models = initialize_models(X, Y[0], Y[1], Y[2])
+    list_of_models = initialize_models(X, *Y)
     assert len(Y) == len(list_of_models)
     model_list_gp = ModelListGP(*[model for model in list_of_models])
 
