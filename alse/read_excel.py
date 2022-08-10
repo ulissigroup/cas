ec0d7e2afba505420510a31ce33b1c7c5f568c59
Leo-wz <leo1839@icloud.com>
2022-08-10 15:47:16 +0000

    improved read excel

--- alse/read_excel.py
+++ alse/read_excel.py
@@ -1,6 +1,5 @@
 import torch, pandas
 
-
 def read_excel(file_path, x_names, y_names):
                                                         # NaN to 0
     consolidated_data = pandas.read_excel(f"{file_path}").fillna(0).loc
@@ -16,6 +15,4 @@ def read_excel(file_path, x_names, y_names):
 
     X = torch.stack(tuple(input_param), -1)
     return X, *output_param
-
-X, pow_cap, width = read_excel(file_path="/home/jovyan/shared-scratch/leo/alse/test_data/trial.xlsx",x_names=["P (W)", "V (mm/min)"], y_names=["powder capt %", "wth"])
 
