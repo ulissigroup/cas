c2a1b5f1ecb3aabac30203319f77ebe861f87747
LoryWang <xiaoxia3@andrew.cmu.edu>
2022-08-11 19:12:31 +0000

    revert to previous generate_n_test_points method

--- alse/alse.py
+++ alse/alse.py
@@ -82,19 +82,12 @@ class alse:
             train_x_temp = torch.cat((train_x_temp, x_next))
             
             for i in range(len(self.model_type)):
-                # sampler = SobolQMCNormalSampler(512) # This number somehow affects the
-                                                     # number of samples in optimize_acqf
-                # Add fantasy models to list
+                y_on_x_next = model_list.models[i](x_next).loc.unsqueeze(-1)
+
+                train_y_temp[i] = torch.cat((train_y_temp[i], y_on_x_next))
                 list_of_models_temp.append(
-                    model_list.models[i].fantasize(train_x_temp, self.eci.sampler)
+                    fit_gp_model(self.model_type[i], train_x_temp, train_y_temp[i])
                 )
-
-                # y_on_x_next = model_list.models[i](x_next).loc.unsqueeze(-1)
-
-                # train_y_temp[i] = torch.cat((train_y_temp[i], y_on_x_next))
-                # list_of_models_temp.append(
-                #     fit_gp_model(self.model_type[i], train_x_temp, train_y_temp[i])
-                # )
         self.next_batch_test_point = unnormalize(train_x_temp[-num_points:], self.x_bounds)
         return self.next_batch_test_point
     
